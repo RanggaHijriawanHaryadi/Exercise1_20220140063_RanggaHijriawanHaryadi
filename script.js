325315eff56edd02e18949bd00aa399b3030ad93
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2024-04-02 09:58:24 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function showAlert(){
+    alert(Date(),tampil(),show())
+    Date()
+
+    let tanggal = new Date();
+    document.getElementById("hasil").innerHTML = tanggal;
+
+    function tampil() {
+        alert(" Mobil adalah kendaraan roda empat yang digerakkan dengan tenaga mesin dengan bahan bakar bensin atau solar yang mempunyai bentuk tertentu")
+    } 
+
+    function show(){
+        alert("sebagai alat transportasi yang digunakan untuk mengangkut manusia dalam skala sedang")
+
+        
+    }
+}
